63a072377dd7417d7d58171249e98fe712b1def3
Thapakornd <89478647+Thapakornd@users.noreply.github.com>
2022-05-16 18:57:55 +0700

    Create function read data to tabel

---
+++ function.php
@@ -0,0 +1,31 @@
+<?php 
+
+    define('DB_server','localhost');
+    define('DB_user','root');
+    define('DB_pass','');
+    define('DB_name','user_db');
+
+    class DB_con{
+        function __construct()
+        {
+            $conn = mysqli_connect(DB_server ,DB_user ,DB_pass ,DB_name);
+            $this->dbcon = $conn;
+
+            if(mysqli_connect_errno()){
+                echo "Failed to connect : ". mysqli_connect_error();
+            }
+        }
+
+        public function insert($user_id,$user_email,$user_pass){
+            $result = mysqli_query($this->dbcon, "INSERT INTO users(user_id,user_email,user_pass) VALUES ($user_id,$user_email,$user_pass)");
+            return $result;
+        }
+
+        public function fetchdata(){
+            $result = mysqli_query($this->dbcon,"SELECT * FROM users");
+            return $result;
+        }
+    }
+
+
+?>
--- index.php
+++ index.php
@@ -4,9 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CRUD test system</title>
 </head>
 <body>
-    
+    <table>
+        <thead>
+            <th>#</th>
+            <th>User ID</th>
+            <th>User Email</th>
+            <th>User Password</th>
+            <th>date</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </thead>
+        <tbody>
+            <?php 
+                
+                include_once('function.php');
+                $fetchdata = new DB_con();
+                $sql = $fetchdata->fetchdata();
+
+                while($row = mysqli_fetch_array($sql)){
+            ?>
+            
+                <tr>
+                    <td><?php echo $row['id']?></td>
+                    <td><?php echo $row['user_id']?></td>
+                    <td><?php echo $row['user_email']?></td>
+                    <td><?php echo $row['user_pass']?></td>
+                    <td><?php echo $row['date']?></td>
+                </tr>
+            <?php 
+                }
+            ?>
+        </tbody>
+    </table>
 </body>
 </html>
