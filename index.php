94da2f4d2a1f65ec2a675df8d43a533dc233bd2c
Thapakornd <89478647+Thapakornd@users.noreply.github.com>
2022-05-17 18:01:27 +0700

    update

--- index.php
+++ index.php
@@ -5,39 +5,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD test system</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <table>
-        <thead>
-            <th>#</th>
-            <th>User ID</th>
-            <th>User Email</th>
-            <th>User Password</th>
-            <th>date</th>
-            <th>Edit</th>
-            <th>Delete</th>
-        </thead>
-        <tbody>
-            <?php 
-                
-                include_once('function.php');
-                $fetchdata = new DB_con();
-                $sql = $fetchdata->fetchdata();
+    <div class="container-md">
+    <table class="table">
+            <thead>
+                <th>#</th>
+                <th>User ID</th>
+                <th>User Email</th>
+                <th>User Password</th>
+                <th>date</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </thead>
+            <tbody>
+                <?php 
+                    
+                    include_once('function.php');
+                    $fetchdata = new DB_con();
+                    $sql = $fetchdata->fetchdata();
 
-                while($row = mysqli_fetch_array($sql)){
-            ?>
-            
-                <tr>
-                    <td><?php echo $row['id']?></td>
-                    <td><?php echo $row['user_id']?></td>
-                    <td><?php echo $row['user_email']?></td>
-                    <td><?php echo $row['user_pass']?></td>
-                    <td><?php echo $row['date']?></td>
-                </tr>
-            <?php 
-                }
-            ?>
-        </tbody>
-    </table>
+                    while($row = mysqli_fetch_array($sql)){
+                ?>
+                
+                    <tr>
+                        <td><?php echo $row['id']?></td>
+                        <td><?php echo $row['user_id']?></td>
+                        <td><?php echo $row['user_email']?></td>
+                        <td><?php echo $row['user_pass']?></td>
+                        <td><?php echo $row['date']?></td>
+                        <td><a href="update.php?id=<?php echo $row['user_id']; ?>" class="btn btn-primary">Edit</a></td>
+                        <td><a href="delete.php?id=<?php echo $row['user_id']; ?>" class="btn btn-danger">Delete</a></td>
+                    </tr>
+                <?php 
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
 </body>
 </html>
